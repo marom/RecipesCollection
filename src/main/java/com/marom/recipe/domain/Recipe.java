d77831e03004eda38294035358d00e073a4b54d8
MaroM <maro.muszynski@gmail.com>
2019-02-24 13:20:48 +0100

    add ManyToMany JPA relationship between Recipe and Category

--- src/main/java/com/marom/recipe/domain/Recipe.java
+++ src/main/java/com/marom/recipe/domain/Recipe.java
@@ -17,8 +17,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -32,6 +30,11 @@ public class Recipe {
     @Enumerated(value = EnumType.STRING)
     private Dificulty dificulty;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category" ,
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -127,5 +130,13 @@ public class Recipe {
 
     public void setDificulty(Dificulty dificulty) {
         this.dificulty = dificulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
