a74184bc5a7aa8bc8675404d2800f25e06bb9715
MaroM <maro.muszynski@gmail.com>
2019-02-24 12:30:22 +0100

    add UnitOfMeasure entity

---
+++ src/main/java/com/marom/recipe/domain/UnitOfMeasure.java
@@ -0,0 +1,29 @@
+package com.marom.recipe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String unitOfMeasure;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(String unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+}
