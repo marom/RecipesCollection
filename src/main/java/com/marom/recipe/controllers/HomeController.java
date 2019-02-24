f56c1efdbedc28310c05e3d7f50f2d16d37b0515
MaroM <maro.muszynski@gmail.com>
2019-02-24 15:59:52 +0100

    add method to the controller to list all recipes.

--- src/main/java/com/marom/recipe/controllers/HomeController.java
+++ src/main/java/com/marom/recipe/controllers/HomeController.java
@@ -1,37 +1,29 @@
 package com.marom.recipe.controllers;
 
-import com.marom.recipe.domain.Category;
-import com.marom.recipe.domain.UnitOfMeasure;
-import com.marom.recipe.repositories.CategoryRepository;
-import com.marom.recipe.repositories.UnitOfMeasureRepository;
+import com.marom.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping({""})
 public class HomeController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+   private RecipeService recipeService;
 
-    public HomeController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public HomeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @GetMapping
     public String home() {
-        System.out.println("change XXXXX");
-
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("Mexican");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Pinch");
-
-        System.out.println(categoryOptional.get().getId());
-        System.out.println(unitOfMeasureOptional.get().getId());
-
         return "index";
+    }
+
+    @GetMapping("/recipes")
+    public String recipesList(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
+        return "recipes";
     }
 }
