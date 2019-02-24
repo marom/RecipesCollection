e7a585bedf006c3ceb3eb8091c47da65d7a2a6fe
MaroM <maro.muszynski@gmail.com>
2019-02-24 15:58:57 +0100

    add @Lob annotation to directions to extend size >255. initialize ingredients and categories to empty hashsets to avoid NPE when data are loaded at startup. fix typo in Difficulty class name and variables

--- src/main/java/com/marom/recipe/domain/Recipe.java
+++ src/main/java/com/marom/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.marom.recipe.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,10 +17,12 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
@@ -28,13 +31,13 @@ public class Recipe {
     private Notes notes;
 
     @Enumerated(value = EnumType.STRING)
-    private Dificulty dificulty;
+    private Difficulty difficulty;
 
     @ManyToMany
     @JoinTable(name = "recipe_category" ,
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -124,12 +127,12 @@ public class Recipe {
         this.notes = notes;
     }
 
-    public Dificulty getDificulty() {
-        return dificulty;
+    public Difficulty getDifficulty() {
+        return difficulty;
     }
 
-    public void setDificulty(Dificulty dificulty) {
-        this.dificulty = dificulty;
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Set<Category> getCategories() {
