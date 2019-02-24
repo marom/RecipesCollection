8db7dd57f78a96bf9a571a94a6878b3949f1519e
MaroM <maro.muszynski@gmail.com>
2019-02-24 14:55:51 +0100

    fix type in the class name

--- src/main/java/com/marom/recipe/repositories/CategoryRepositoey.java
+++
@@ -1,7 +0,0 @@
-package com.marom.recipe.repositories;
-
-import com.marom.recipe.domain.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepositoey extends CrudRepository<Category, Long> {
-}
--- src/main/java/com/marom/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/marom/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.marom.recipe.repositories;
 import com.marom.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
