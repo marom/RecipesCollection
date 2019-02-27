e5c2c49b55e5f730f473a567c84986faf40a08e9
MaroM <maro.muszynski@gmail.com>
2019-02-27 14:40:20 +0100

    add test for mockito argument capture example

--- src/test/java/com/marom/recipe/controllers/HomeControllerTest.java
+++ src/test/java/com/marom/recipe/controllers/HomeControllerTest.java
@@ -1,11 +1,16 @@
 package com.marom.recipe.controllers;
 
+import com.marom.recipe.domain.Recipe;
 import com.marom.recipe.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -35,5 +40,33 @@ public class HomeControllerTest {
 
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+
+    @Test
+    public void argumentCapture() {
+
+        //given
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(recipe1);
+        recipes.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewName = homeController.recipesList(model);
+
+        //then
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertThat(setInController.size(), is(2));
     }
 }
