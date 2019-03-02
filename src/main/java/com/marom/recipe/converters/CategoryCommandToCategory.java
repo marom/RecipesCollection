66834ffc3fa99768f623d35501354d28f2f054e4
MaroM <maro.muszynski@gmail.com>
2019-03-02 19:37:08 +0100

    add converters for command classes

---
+++ src/main/java/com/marom/recipe/converters/CategoryCommandToCategory.java
@@ -0,0 +1,30 @@
+package com.marom.recipe.converters;
+
+
+import com.marom.recipe.commands.CategoryCommand;
+import com.marom.recipe.domain.Category;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category>{
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Category category = new Category();
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+        return category;
+    }
+}
