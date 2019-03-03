0679c7fb6aa544f70903411e7357dd62ffc38c40
MaroM <maro.muszynski@gmail.com>
2019-03-03 12:30:33 +0100

    add create a new Recipe functionality

--- src/main/java/com/marom/recipe/controllers/RecipeController.java
+++ src/main/java/com/marom/recipe/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package com.marom.recipe.controllers;
 
+import com.marom.recipe.commands.RecipeCommand;
 import com.marom.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -20,5 +20,20 @@ public class RecipeController {
 
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @GetMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/addRecipe";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+
+        RecipeCommand savedRecipe = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedRecipe.getId();
     }
 }
