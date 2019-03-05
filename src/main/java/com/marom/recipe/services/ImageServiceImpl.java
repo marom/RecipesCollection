bb7c3b20b6e998f8ee01103854012e3612299d55
MaroM <maro.muszynski@gmail.com>
2019-03-05 13:25:12 +0100

    persist an uploaded image to the database

--- src/main/java/com/marom/recipe/services/ImageServiceImpl.java
+++ src/main/java/com/marom/recipe/services/ImageServiceImpl.java
@@ -1,16 +1,46 @@
 package com.marom.recipe.services;
 
+import com.marom.recipe.domain.Recipe;
+import com.marom.recipe.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl( RecipeRepository recipeService) {
+
+        this.recipeRepository = recipeService;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+            e.printStackTrace();
+        }
         log.debug("Saved a file");
     }
 }
