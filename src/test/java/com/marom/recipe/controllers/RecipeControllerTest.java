f833d677051e000552e30bf5b259c02fa1f1038b
MaroM <maro.muszynski@gmail.com>
2019-03-07 18:18:53 +0100

    handle NumberformatException in a nice way by displaying custom page with a message

--- src/test/java/com/marom/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/marom/recipe/controllers/RecipeControllerTest.java
@@ -105,4 +105,14 @@ public class RecipeControllerTest {
                 .andExpect(view().name("404error"));
     }
 
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+
+        when(recipeService.findById(anyLong())).thenThrow(NumberFormatException.class);
+
+        mockMvc.perform(get("/recipe/abc/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
     }
